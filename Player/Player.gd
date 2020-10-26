8da02fb831118271026039087efa4a62ccd092fe
William Tilton <william.tilton@caci.com>
2020-10-26 00:04:26 -0700

    Shit

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,9 +1,11 @@
 extends KinematicBody2D
 
 var velocity = Vector2.ZERO
+var roll_vector = Vector2.DOWN
 
 const player_speed = 90
 const acceleration = 450
+const roll_speed = 145
 const friction = 450
 
 onready var animation = $AnimationPlayer
@@ -25,6 +27,8 @@ func _physics_process(delta):
 			move_state(delta)
 		ATTACK:
 			attack_state(delta)
+		ROLL:
+			roll_state(delta)
 		
 
 func move_state(delta):
@@ -33,20 +37,24 @@ func move_state(delta):
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
 	if input_vector != Vector2.ZERO:
+		roll_vector = input_vector
 		animation_tree.set("parameters/Idle/blend_position", input_vector)
 		animation_tree.set("parameters/Run/blend_position", input_vector)
 		animation_tree.set("parameters/Attack/blend_position", input_vector)
 		animation_state.travel("Run")
 		velocity = velocity.move_toward(input_vector * player_speed, acceleration * delta)
-		print(velocity)
+		
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
 		animation_state.travel("Idle")
-		
-	velocity = move_and_slide(velocity)
+	
+	move()
 	
 	if Input.is_action_just_pressed("attack"):
 		state = ATTACK
+		
+	if Input.is_action_just_pressed("roll"):
+		state = ROLL
 		
 
 func attack_state(delta):
@@ -54,3 +62,16 @@ func attack_state(delta):
 
 func attack_complete():
 	state = MOVE
+	
+func roll_state(delta):
+	velocity = roll_vector * roll_speed
+	animation_state.travel("Roll")
+	move()
+
+func roll_complete():
+	state = MOVE
+	
+func move():
+	velocity = move_and_slide(velocity)
+
+
