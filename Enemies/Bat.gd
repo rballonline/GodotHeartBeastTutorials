d378c36619cdbf95d973ff168308e468f3dbe920
William Tilton <william.tilton@caci.com>
2020-10-27 20:34:57 -0700

    End of 15

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 var knockback = Vector2.ZERO
-const death_effect = preload("res://Effects/BatDeathEffect.tscn")
+const BatDeathEffect = preload("res://Effects/BatDeathEffect.tscn")
 
 onready var stats = $Stats
 
@@ -14,6 +14,8 @@ func _on_HurtBox_area_entered(area):
 	print(stats.health)
 	knockback = area.knockback_vector * 120
 
-func _on_Stats_no_health():
-	print("dead")
+func _on_Stats_no_health():	
 	queue_free()
+	var death_effect = BatDeathEffect.instance()
+	get_parent().add_child(death_effect)
+	death_effect.global_position = global_position
