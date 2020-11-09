b28e04693bd58337d67769d1ef491458f9b940e5
William Tilton <william.tilton@caci.com>
2020-11-09 14:58:42 -0800

    Quarter way through https://www.youtube.com/watch?v=rdUgf6r7w2Q&t=595s

--- UI/Inventory.gd
+++ UI/Inventory.gd
@@ -1,4 +1,24 @@
 extends Resource
 class_name Inventory
 
-signal item_changed(indexes)
+signal items_changed(indexes)
+
+export(Array, Resource) var items = [
+	null, null, null,null, null, null,null, null, null
+]
+
+func set_item(index, item):
+	var previous_item = items[index]
+	items[index] = item
+	emit_signal("items_changed", [index])
+	return previous_item
+	
+func swap_item(index, target_index):
+	var i = items[index]
+	var t = items[target_index];
+	items[index] = t
+	items[target_index] = i
+	emit_signal("items_changed", [index, target_index])
+	
+func remove_item(index):
+	return set_item(index, null)
