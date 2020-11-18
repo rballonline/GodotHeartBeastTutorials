37e7a9c091415ad375e57a601e45f4599870cbae
William Tilton <william.tilton@caci.com>
2020-11-18 13:38:55 -0800

    Whatever

--- Effects/GrassEffect.gd
+++ Effects/GrassEffect.gd
@@ -5,7 +5,7 @@ var Coin = preload("res://World/Coin.tscn")
 func reveal():
 	var number = rand_range(1, 3)
 	print(str(number))
-	if number <= 2:
-		var coin = Coin.instance()
-		get_parent().add_child(coin)
-		coin.play(position + Vector2(8, 8), get_global_transform_with_canvas().get_origin() + Vector2(8, 8))
+#	if number <= 2:
+	var coin = Coin.instance()
+	get_parent().add_child(coin)
+	coin.play(position + Vector2(8, 8), get_global_transform_with_canvas().get_origin() + Vector2(8, 8))
