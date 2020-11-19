e6e1c04d0ebd4a249b25d1c7a80b5298be779430
William Tilton <william.tilton@caci.com>
2020-11-18 17:35:51 -0800

    Making some paths, moved Santa, seeing what we can do here

--- World/Coin.gd
+++ World/Coin.gd
@@ -1,23 +1,15 @@
 extends Node2D
 
 onready var tween = $Tween
-onready var collection = $CanvasLayer/CoinCollection
-onready var coin = $CoinAnchor
+onready var coin = $CanvasLayer/AnimatedCoin
 onready var audio = $AudioStreamPlayer
 
 export(int) var amount = 1
 
-func play(position, global_position):
+func play(position):
 	print("Coin play()")
-	coin.position = position
-	collection.set_global_position(global_position)
-	audio.play()
-	tween.interpolate_property(collection, "modulate:a", 0.0, 1.0, 0.5, Tween.EASE_IN, Tween.TRANS_LINEAR)
-	tween.start()
-	yield(tween, "tween_completed")
-	coin.visible = false
-	collection.visible = true
-	tween.interpolate_property(collection, "position", collection.position, Vector2(288, 8), 0.5, Tween.EASE_IN, Tween.TRANS_LINEAR)
+	coin.set_global_position(position)
+	tween.interpolate_property(coin, "position", coin.position, Vector2(288, 8), 0.5, Tween.EASE_IN, Tween.TRANS_LINEAR)
 	tween.start()
 	yield(tween, "tween_completed")
 	queue_free()
